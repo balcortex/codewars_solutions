3544525ce4b4cd6490bd845888c9b5a3e02d5da4
Bal Cortex <balcortex@hotmail.com>
2024-04-02 22:04:54 -0600

    add: sql problem

---
+++ sql/7kyu/bytes_in_string_from_bits.sql
@@ -0,0 +1,6 @@
+SELECT LENGTH(CAST(number1 AS text)) AS octnum1,
+    LENGTH(CAST(number2 AS text)) AS octnum2,
+    LENGTH(CAST(number3 AS text)) AS octnum3,
+    LENGTH(CAST(number4 AS text)) AS octnum4,
+    LENGTH(CAST(number5 AS text)) AS octnum5
+FROM numbers
